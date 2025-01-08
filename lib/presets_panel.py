4788f38a07d7fc5037ade1cf417e094cadaa835a
anoircp <anoircp@gmail.com>
2025-01-08 18:57:21 +0100

    update | loading data

--- lib/presets_panel.py
+++ lib/presets_panel.py
@@ -8,10 +8,14 @@ class PresetApp:
         self.root = root
         self.root.title("Preset Selector GUI")
 
-        # Load settings from settings.ini
+        # Load settings
         self.config = configparser.ConfigParser()
         self.config.read('../settings.ini')
         self.presets = int(self.config['Settings']['presets'])
+        self.geometry = self.config['Settings']['geometry']
+
+        # application window
+        self.root.geometry(self.geometry)
 
         # Create and place the dropdown menu
         self.preset_label = tk.Label(root, text="Select Preset:")
@@ -24,27 +28,16 @@ class PresetApp:
         self.preset_dropdown.current(0)
         self.preset_dropdown.pack(pady=10)
 
-        # Create and place the "Generate" button
+        # Create and place
         self.generate_button = tk.Button(root, text="Generate", command=self.generate)
         self.generate_button.pack(pady=10)
 
-        # Create and place the "Confirm" button
-        self.confirm_button = tk.Button(root, text="Confirm", command=self.confirm)
-        self.confirm_button.pack(pady=10)
-        self.confirm_button.config(state=tk.DISABLED)  # Initially disabled
-
     def generate(self):
         selected_preset = self.preset_var.get()
-        # Call generator.py with the selected preset
-        subprocess.run(['python3', 'generator.py', selected_preset])
-        messagebox.showinfo("Generate", f"Generated folders for {selected_preset}")
-        # Enable the "Confirm" button after generation
-        self.confirm_button.config(state=tk.NORMAL)
-
-    def confirm(self):
-        # Perform the confirm action
-        messagebox.showinfo("Confirm", "Confirmed the generated folders")
-        self.confirm_button.config(state=tk.DISABLED)  # Disable the button after confirming
+        # Call generator.py with the selected presets
+        subprocess.run(['python3', 'generate.py', selected_preset])
+        messagebox.showinfo("Generate", "Generated Content!")
+        self.root.destroy()  # Close the application
 
 if __name__ == "__main__":
     root = tk.Tk()
